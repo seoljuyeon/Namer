6f80f93e263a884b62844eb3194121033c4ef82e
seoljuyeon <sxxljxxxxn@gmail.com>
2024-03-13 21:55:34 +0900

    init

---
+++ server/src/main/java/org/seoljuyeon/namer/NamerApplication.java
@@ -0,0 +1,13 @@
+package org.seoljuyeon.namer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NamerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NamerApplication.class, args);
+    }
+
+}
---
+++ server/src/test/java/org/seoljuyeon/namer/NamerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.seoljuyeon.namer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NamerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
